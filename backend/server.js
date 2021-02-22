f5e392d246fdfef0474ad4af6455d48aec5e16e8
Lap-032 <udhaysekar@gmail.com>
2021-02-22 12:39:12 +0530

    git test RMS2

--- backend/server.js
+++ backend/server.js
@@ -50,5 +50,5 @@ res.send('s');
 app.use(errorHandler);
 
 // start server
-const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
+const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 8000;
 app.listen(port, () => console.log('Server listening on port ' + port));
